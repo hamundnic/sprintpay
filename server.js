c2790b4ce437892ca3a3cbc780200a73a577b0f2
ZweiteHookage <hamletleonardo@gmail.com>
2021-03-27 15:46:12 +0100

    express-1

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-process.env.PORT = 5000;
+
 app.use(express.static(__dirname + '/dist/sprint-pay'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +
         '/dist/sprint-pay/index.html'));
 });
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT);
