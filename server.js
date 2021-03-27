0a7faf6f14a7e71ad689e64bfb31540fecb2f705
ZweiteHookage <hamletleonardo@gmail.com>
2021-03-27 15:30:08 +0100

    express-1

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+
 const app = express();
 process.env.PORT = 5000;
 app.use(express.static(__dirname + '/dist/sprint-pay'));
@@ -7,4 +9,4 @@ app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +
         '/dist/sprint-pay/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
